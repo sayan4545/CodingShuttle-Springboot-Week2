ccf96793791d49ba1dce761a9142d260ff308c60
sayan4545 <sayanchatterjee3010@gmail.com>
2025-02-08 15:36:40 +0530

    added lombok depenedency, modularized the code

--- src/main/java/com/springbootweb/springboot/web/entities/EmployeeEntity.java
+++ src/main/java/com/springbootweb/springboot/web/entities/EmployeeEntity.java
@@ -3,59 +3,26 @@ package com.springbootweb.springboot.web.entities;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "employee")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
-    @JsonProperty("isActive")
     private boolean isActive;
     private LocalDate doj;
 
-    public EmployeeEntity(long id, String name, boolean isActive, LocalDate doj) {
-        this.id = id;
-        this.name = name;
-        this.isActive = isActive;
-        this.doj = doj;
-    }
 
-    public EmployeeEntity() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public LocalDate getDoj() {
-        return doj;
-    }
-
-    public void setDoj(LocalDate doj) {
-        this.doj = doj;
-    }
 }
